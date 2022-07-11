31c887831c659490f45ecbe353d8cf650124513d
lxw <2332425872@qq.com>
2022-07-11 11:10:46 +0800

    second commit

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -3,6 +3,9 @@ package com.example.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lxw
+ */
 @SpringBootApplication
 public class TestApplication {
 
